d8a8d85fa35ba88a36ae4cc4310ca30f609e6d9a
m.a.erdyakov <m.a.erdyakov@tinkoff.ru>
2023-11-25 14:26:53 +0300

    удалил математические функции

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
 
-int multiply(int a, int b) {
-    return a * b;
-}
-
-int sum(int a, int b) {
-    return a + b;
-}
-
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
